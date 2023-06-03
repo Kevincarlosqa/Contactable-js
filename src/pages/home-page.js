157a85b49c488791261bc3efca9e35bc8fd65703
Kevin Quispe <kevincarlosqa@gmail.com>
2023-06-03 17:58:06 -0500

    add: listenLogout function

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,3 +1,6 @@
+import { logout } from "../services/sessions-service.js";
+import DOMHandler from "../dom-handler.js";
+import loginPage from "./login-page.js";
 import STORE from "../store.js";
 
 function render() {
@@ -5,7 +8,7 @@ function render() {
     <header class="header">
       <navbar class="navbar">
         <h1 class="heading heading--lg mb-2">Contactable</h1>
-        <a>Logout</a>
+        <a class="js-logout">Logout</a>
       </navbar>
     </header>
     <main class="section">
@@ -16,13 +19,30 @@ function render() {
     </main>
   `
 }
+
+function listenLogout() {
+  const a = document.querySelector(".js-logout")
+  a.addEventListener("click", async (event) => {
+    event.preventDefault()
+    try {
+      await logout()
+      loginPage.state.loginError = null
+      DOMHandler.load(loginPage)
+
+    } catch (error) {
+      console.log(error);
+    }
+  })
+}
 const homePage = {
   toString() {
     // console.log(STORE.contacts);
     // console.log(STORE.favorites);
     return render()
   },
-  addListeners() {},
+  addListeners() {
+    listenLogout()
+  },
 }
 
 export default homePage
